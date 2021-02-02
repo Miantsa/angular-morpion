269a1b9e3a23681efccfa56c6ffbdba3ad13c342
Miantsa <miantsamiantsarazakamanantsoa@gmail.com>
2021-02-02 17:06:39 +0300

    adding event Listener on game platform

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { table } from 'console';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent implements OnInit {
+  // table: HTMLTableElement = document.querySelector('#tableID');
+ matrice; 
+  // get all rows in the first table body
+   //rows = table
+  constructor() { }
+
+  ngOnInit(): void {
+    this.matrice=document.querySelectorAll('.case');
+    for (let val of this.matrice) {
+      val.addEventListener('click',(e)=>{
+        console.log('click');
+
+      });
+      //console.log(val); // prints values: 10, 20, 30, 40
+    }
+  }
+  addToken(value){
+    const nb=this.matrice.length;
+    console.log("hey"+nb);
+   
+  }
+}
