5b5e6ec3a6fbafbde6d71f810bc9ef9346cd43e9
Miantsa <miantsamiantsarazakamanantsoa@gmail.com>
2021-02-05 11:09:43 +0300

    finish player vs player game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,16 @@ import { Component ,OnInit} from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'morpion';
-  player1Title;
-  player2Title;
+  player1Title:any;
+  player2Title:any;
+  modals:any;
+  winnerInfo:String="Win !";
+  obj = { '1': 'Player 1', '-1': 'Player 2' };
+  matrix2D;
   ngOnInit():void{
     this.player1Title=document.querySelector('.player1');
     this.player2Title=document.querySelector('.player2');
+    this.modals=document.querySelector('.modals');
   }
   test(val){
     if(val.checked){
@@ -22,7 +27,7 @@ export class AppComponent implements OnInit {
     console.log(val.checked);
   }
   childToParent(value){
-    const borderStyle="greenyellow 3px solid";
+    const borderStyle="greenyellow 1px solid";
     const borderDefaultStyle= "1px white solid";
    if(value==1){
      this.player1Title.style.borderBottom =borderStyle;
@@ -33,5 +38,21 @@ export class AppComponent implements OnInit {
     this.player1Title.style.borderBottom =borderDefaultStyle;
    }
    
+  }
+  childToParent2(value){
+    this.modals.style.display="flex";
+     this.winnerInfo=this.obj[value.toString()]+ " " +this.winnerInfo;
+  }
+  childToParent3(value){
+    this.matrix2D=value;
+  }
+  replay(){
+    this.modals.style.display="none";
+    for(let i=0;i<this.matrix2D.length;i++){
+      for(let j=0;j<this.matrix2D[i].length;j++){
+        this.matrix2D[i][j].innerHTML="";
+      }
+    }
+    this.winnerInfo="Win!";
   }
 }
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -7,56 +7,65 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
-  // table: HTMLTableElement = document.querySelector('#tableID');
-  matrice;
-  matrice2D = [];
-  // get all rows in the first table body
-  //rows = table
-  
-  X:number =1;
-  O:number =-1;
+
+  matrix;
+  matrix2D = [];
+  X: number = 1;
+  O: number = -1;
   turn: number = this.X;
-  obj = { '1': 'X', '-1':'O' };
+  obj = { '1': 'X', '-1': 'O' };
+  //share data to app.component
   @Output() childToParent = new EventEmitter<String>();
-  constructor() { 
-   
+  @Output() childToParent2 = new EventEmitter<String>();
+  @Output() childToParent3 = new EventEmitter<String>();
+  constructor() {
+
 
   }
 
   ngOnInit(): void {
-    
 
-    this.matrice = document.querySelectorAll('.case');
-   
-    this.matrice = Array.from(this.matrice);
-    console.log(this.matrice)
-    while (this.matrice.length) this.matrice2D.push(this.matrice.splice(0, 3));
-    console.log(this.matrice2D);
 
+    this.matrix = document.querySelectorAll('.case');
+
+    this.matrix = Array.from(this.matrix);
+    console.log(this.matrix)
+    while (this.matrix.length) this.matrix2D.push(this.matrix.splice(0, 3));
+    console.log(this.matrix2D);
+    this.sendToParent3(this.matrix2D);
     this.sendToParent(this.turn);
-    for (let i = 0; i < this.matrice2D.length; i++) {
+    for (let i = 0; i < this.matrix2D.length; i++) {
 
-      for (let j = 0; j < this.matrice2D[i].length; j++) {
-        const val = this.matrice2D[i][j];
+      for (let j = 0; j < this.matrix2D[i].length; j++) {
+        const val = this.matrix2D[i][j];
         val.addEventListener('click', () => {
-         // console.log('click' + val.cellIndex);
-          console.log(val.innerHTML.length)
-         // val.innerHTML+="X";
-          if(val.innerHTML.length==0){
-           
-            val.innerHTML=this.obj[this.turn+''];
-            this.turn=-this.turn;
+        //  console.log(val.innerHTML.length)
+          if (val.innerHTML.length == 0) {
+            val.innerHTML = this.obj[this.turn + ''];
+            this.turn = -this.turn;
             this.sendToParent(this.turn);
+            const node = new Node(this.stringToNumberMatrix(this.matrix2D), this.turn);
+            const result = node.evaluation();
+            if (result == 0) {
+            } else if (result == 100 * this.X) {
+              console.log("X gagne");
+              this.turn=this.X;
+              this.sendToParent2(this.X);
+            } else if (result == 100 * this.O) {
+              console.log("O gagne");
+              this.turn=this.X;
+              this.sendToParent2(this.O);
+            }
+
+            this.changeCaseColor(val);
           }
-         
-          this.changeCaseColor(val);
         });
       }
     }
 
   }
   addToken(value) {
-    const nb = this.matrice.length;
+    const nb = this.matrix.length;
     console.log("hey" + nb);
 
   }
@@ -66,16 +75,73 @@ export class GameComponent implements OnInit {
     else if (value.innerHTML == "O")
       value.style.color = "#0dcaf0";
   }
-  verifyGameStatus(mat) {
 
-  }
   vsPlayer() {
 
   }
   vsIa() {
 
   }
-  sendToParent(turn){
+  sendToParent(turn) {
     this.childToParent.emit(turn);
+  }
+  sendToParent2(winner) {
+    this.childToParent2.emit(winner);
+  }
+  sendToParent3(matrix) {
+    this.childToParent3.emit(matrix);
+  }
+  stringToNumberMatrix(mat: any) {
+    let result: number[][] = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+    for (let i = 0; i < mat.length; i++) {
+      for (let j = 0; j < mat[i].length; j++) {
+        if (mat[i][j].innerHTML == "X") result[i][j] = this.X;
+        else if (mat[i][j].innerHTML == "O") result[i][j] = this.O;
+        else if (mat[i][j].innerHTML == "" || mat[i][j].innerHTML == undefined) result[i][j] = 0;
+      }
     }
+    console.log(result)
+    return result;
+  }
+  numberMatrixToGamePlateform(fromNode, toGui) {
+    for (let i = 0; i < fromNode.length; i++) {
+      for (let j = 0; j < fromNode[i].length; j++) {
+        if (fromNode[i][j] == this.X) toGui[i][j].innerHTML = "X";
+        else if (fromNode[i][j] == this.O) toGui[i][j].innerHTML = "O";
+        else if (fromNode[i][j] == 0) toGui[i][j].innerHTML = "";
+      }
+    }
+  }
+}
+class Node {
+  matrix: number[][] = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+  turn: number;
+  constructor(mat: any, turn) {
+    this.matrix = mat;
+    this.turn = turn;
+  }
+
+  evaluation() {
+    //vertical
+    for (let i = 0; i < 3; i++)
+      if (this.matrix[i][0] == this.matrix[i][1] && this.matrix[i][1] == this.matrix[i][2]) return 100 * this.matrix[i][0];
+    //horizontal
+    for (let i = 0; i < 3; i++)
+      if (this.matrix[0][i] == this.matrix[1][i] && this.matrix[1][i] == this.matrix[2][i]) return 100 * this.matrix[0][i];
+    //diagonal \
+    if (this.matrix[0][0] == this.matrix[1][1] && this.matrix[1][1] == this.matrix[2][2]) return 100 * this.matrix[1][1];
+    //diagonal /
+    if (this.matrix[0][2] == this.matrix[1][1] && this.matrix[1][1] == this.matrix[2][0]) return 100 * this.matrix[1][1];
+    return 0;
+  }
+  isFinished() {
+    if (this.evaluation() != 0) return true;
+    //getSucc().size()==0; 
+    for (let i = 0; i < 3; i++)
+      for (let j = 0; j < 3; j++)
+        if (this.matrix[i][j] == 0) return false; 
+
+    return true;
+
+  }
 }
